fd27536ce93220ff4f4648adf430bc09d80534b7
Mattias Källström <mattias-kallstrom@hotmail.com>
2022-01-15 18:29:09 +0100

    plsss

--- src/components/About.js
+++ src/components/About.js
@@ -7,23 +7,23 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Mattias Källström
+            <br className="hidden lg:inline-block" />Front-end focused developer
           </h1>
-          <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray">
-            Frontend focused web developer
-          </h2>
           <p className="mb-8 leading-relaxed">
-            I love building great apps, always down to learn new things.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Contact
+              Work With Me
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              Earlier projects
+              See My Past Work
             </a>
           </div>
         </div>
