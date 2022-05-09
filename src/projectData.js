9065aca0ba5727561526ccd377666949423ea8b5
Mattias <mattias-kallstrom@hotmail.com>
2022-05-09 18:45:10 +0200

    Update

--- src/projectData.js
+++ src/projectData.js
@@ -1,19 +1,26 @@
 export const projects = [
-    {
-      title: "Exam project",
-      subtitle: "React and Firebase",
-      description:
-        "Last examination for my 2 years studying at Changemaker Educations web development programme.",
-      image: require("./assets/ExamsPreview.png"),
-      link: "https://mattiaskallstrom.github.io/Exams/"
-    },
-    {
-      title: "Cookie Clicker",
-      subtitle: "React",
-      description:
-        "Simple cookie clicked game made with React",
-      image: require("./assets/CookiePreview.png"),
-      link: "https://mattiaskallstrom.github.io/Clicker/"
-    },
-  ]
-  
+  {
+    title: "Exam project",
+    subtitle: "React.js and Firebase",
+    description:
+      "Last examination for my 2 years studying at Changemaker Educations web development programme.",
+    image: require("./assets/ExamsPreview.png"),
+    link: "https://mattiaskallstrom.github.io/Exams/"
+  },
+  {
+    title: "Cookie Clicker",
+    subtitle: "React.js",
+    description:
+      "Simple cookie clicked game made with React",
+    image: require("./assets/CookiePreview.png"),
+    link: "https://mattiaskallstrom.github.io/Clicker/"
+  },
+  {
+    title: "Vue Todo",
+    subtitle: "Vue.js",
+    description:
+      "First attempt at learning Vue.js",
+    image: require("./assets/VuePreview.png"),
+    link: "https://mattiaskallstrom.github.io/Vuetodo/"
+  },
+]
